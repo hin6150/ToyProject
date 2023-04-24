ffdecfe1027d75935c53ab9c6e2eeff69e6f96c0
hin6150 <hin6150@gmail.com>
2023-04-24 19:04:40 +0900

    FeedBack Park Feedback 04-24

--- Team-Study/Park/JS_Quiz/Document.js
+++ Team-Study/Park/JS_Quiz/Document.js
@@ -2,6 +2,7 @@
 
 const img = document.createElement("img");
 img.setAttribute("src", "media/Serch.png");
+// 경로 오류
 
 const container = document.querySelector(".container");
 container.append(img);
@@ -13,8 +14,13 @@ boxElements.forEach((names) => {
   names.setAttribute("font-size", "24x");
 });
 
+//names.setAttribute(" ㅏ", "font-size: 24px")
+
 /*3번*/
 const answer = document.querySelector(".box .answer");
-if (answer) {
-  answer.textContent = "네 잘하고있습니다.";
-}
+
+answer.textContent = "네 잘하고있습니다.";
+
+// boxElements.forEach((names) => {
+//   if (names.class=="answer") names.textContent = "네 잘하고있습니다.";
+// });
