9d24beb7108d878d670cdc799f5aeff1c1226e36
pyw424 <parkyw0424@naver.com>
2023-06-22 05:22:19 +0900

    포스터페이지 버튼 아이콘으로 변경(#2)

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -3,36 +3,53 @@ import { FirstPost, SecondPost } from "../Component/Posts/Posts";
 import { Link } from "react-router-dom";
 import "../Detail.css";
 import { FirstText, SecondText } from "../Component/Texts/Texts";
+import {
+  MdHome,
+  MdKeyboardArrowRight,
+  MdKeyboardArrowLeft,
+} from "react-icons/md";
 
 const Detail = () => {
   return (
     <div className="Main">
       <div className="header">
-        <h1 className="mainName">지역 이름</h1>
+        <h1 className="mainName">천안(지역 이름)</h1>
         <Link to={"/"}>
-          <button className="homeButton">홈</button>
+          <MdHome className="homeButton">홈</MdHome>
         </Link>
       </div>
+
       <div className="postList">
         <div className="postOne">
           <p className="left">
-            <button>왼쪽</button>
+            <MdKeyboardArrowLeft>왼쪽</MdKeyboardArrowLeft>
           </p>
           <p className="right">
-            <button>오른쪽</button>
+            <MdKeyboardArrowRight>오른쪽</MdKeyboardArrowRight>
           </p>
           <FirstPost></FirstPost>
-          <FirstText></FirstText>
+          <FirstText>
+            <h1>천안흥타령춤축제</h1>
+            <ul>
+              <li>2023-10-05 ~ 2023-10-09</li>
+              <li> 천안종합운동장 일원</li>
+              <li> http://www.cheonanfestival.com/</li>
+            </ul>
+          </FirstText>
         </div>
         <div className="postTwo">
           <p className="left">
-            <button>왼쪽</button>
+            <MdKeyboardArrowLeft>왼쪽</MdKeyboardArrowLeft>
           </p>
           <p className="right">
-            <button>오른쪽</button>
+            <MdKeyboardArrowRight>오른쪽</MdKeyboardArrowRight>
           </p>
           <SecondPost></SecondPost>
-          <SecondText></SecondText>
+          <SecondText>
+            <h1>블루베리</h1>
+            <br></br>
+            <p>무농약 친환경 재배된 블루베리 천안에서 수확시작</p>
+          </SecondText>
         </div>
       </div>
     </div>
