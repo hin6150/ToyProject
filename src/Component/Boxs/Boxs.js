08bf4f96e98c209cde050a261cd4ce6c42f11283
Chanju03 <chanju030303@naver.com>
2023-06-22 18:40:38 +0900

    전달(#1)

--- src/Component/Boxs/Boxs.js
+++ src/Component/Boxs/Boxs.js
@@ -3,7 +3,7 @@ import "./Boxs.css";
 
 export const Box = (props) => {
   return (
-    <div class="Box" style={{ width: props.size }}>
+    <div class="Box" style={{ width: props.size, height: props.height }}>
       <img src={`./img/${props.url}.png`} alt={props.url} />
       <div class="BoxText">{props.children}</div>
     </div>
---
+++ src/Component/SpecialBox/OutBox.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./OutBox.css";
+
+export const OutBox = () => {
+  return <div class="OutBox"></div>;
+};
