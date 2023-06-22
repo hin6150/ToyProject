6677c18b81e8bd3be3a16a1d948087d171c608e5
Chanju03 <chanju030303@naver.com>
2023-06-22 19:22:48 +0900

    메인페이지 프레임 UI 수정(#1)

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Banner } from "../Component/Banner/Banner";
 import Article from "../Component/Article/Article";
-import { Box, RowTwo, TestRow } from "../Component/Boxs/Boxs";
+import { Box } from "../Component/Boxs/Boxs";
+import { OutBox } from "../Component/SpecialBox/OutBox";
 
 export const Main = () => {
   return (
@@ -10,7 +11,7 @@ export const Main = () => {
         <Banner></Banner>
       </Article>
       <Article>
-        <Box size="36vw" url="mud">
+        <Box size="36vw" url="mud" height="36vw">
           <p>
             BORYEONG
             <br />
@@ -19,7 +20,7 @@ export const Main = () => {
             FESTIVAL
           </p>
         </Box>
-        <Box size="64vw" url="apple">
+        <Box size="64vw" url="apple" height="36vw">
           <p>
             YESAN
             <br />
@@ -28,19 +29,38 @@ export const Main = () => {
         </Box>
       </Article>
       <Article>
-        <Box size="64vw" url="fire">
+        <Box size="64vw" url="fire" height="30vw">
           <p>
             2023 PINNACLE LAND
             <br />
             FIREWORKS FESTIVAL
           </p>
         </Box>
-        <Box size="36vw" url="green">
+        <Box size="36vw" url="green" height="30vw">
           <p>
             GEUMSAN <br />
             PERILLA LEAF
           </p>
         </Box>
+      </Article>
+      <Article>
+        <OutBox></OutBox>
+        <Box size="36vw" url="호두" height="48vw"></Box>
+      </Article>
+      <Article>
+        <Box size="70vw" url="배" height="16vw"></Box>
+        <Box size="30vw" url="Rectangle 32" height="16vw"></Box>
+      </Article>
+      <Article>
+        <Box size="30vw" url="pink22" height="16vw"></Box>
+        <Box size="70vw" url="boryeong" height="16vw"></Box>
+      </Article>
+      <Article>
+        <Box size="70vw" url="festival" height="16vw"></Box>
+        <Box size="30vw" url="pink22" height="16vw"></Box>
+      </Article>
+      <Article>
+        <Box size="100%" url="lastimg" height="100vw"></Box>
       </Article>
     </div>
   );
