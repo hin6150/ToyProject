1de711d65fba518f3bf58f790d3cf8988f7d71e4
hin6150 <hin6150@gmail.com>
2023-06-27 22:59:53 +0900

    상세페이지 케러샐 추가

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -5,9 +5,37 @@ import Header from "../Component/Header/Header";
 import ChangePoster from "../Component/ChangePoster/ChangePoster";
 import "./Detail.css";
 import { useLocation } from "react-router-dom";
-import Coverflow from "react-coverflow";
+
+import { v4 as uuidv4 } from "uuid";
+import Card from "./Card";
+import Carousel from "./Carousel";
 
 const Detail = () => {
+  let cards = [
+    {
+      key: uuidv4(),
+      content: <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg" />,
+    },
+    {
+      key: uuidv4(),
+      content: <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />,
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
+      ),
+    },
+    {
+      key: uuidv4(),
+      content: <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />,
+    },
+    {
+      key: uuidv4(),
+      content: <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />,
+    },
+  ];
+
   const [showFest, setShowFest] = useState(true);
   const [modalOpen, setModalOpen] = useState(false);
   const [mainName, setMainName] = useState("보령");
@@ -23,26 +51,12 @@ const Detail = () => {
       {modalOpen && <Sidebar setModalOpen={setModalOpen} />}
       <Header mainName={mainName} setModalOpen={setModalOpen} />
       <ChangePoster setShowFest={setShowFest} />
-      {showFest ? <ExplainFest /> : <ExplainFood />}
-      {/* <Coverflow width={960} height={480} displayQuantityOfSide={2} navigation={false} enableHeading={false}>
-        <div role="menuitem" tabIndex="0">
-          <img
-            src="[image/path/please_change]"
-            alt="title or description"
-            style={{ display: "block", width: "100%" }}
-          />
+      <div style={{ display: "flex", flex: "1 0 auto" }}>
+        {showFest ? <ExplainFest /> : <ExplainFood />}
+        <div className="carouselBox">
+          <Carousel cards={cards} height="500px" width="30%" margin="0 auto" offset={2} showArrows={false} />
         </div>
-        <img
-          src="[image/path/please_change]"
-          alt="title or description"
-          data-action="http://andyyou.github.io/react-coverflow/"
-        />
-        <img
-          src="[image/path/please_change]"
-          alt="title or description"
-          data-action="http://andyyou.github.io/react-coverflow/"
-        />
-      </Coverflow> */}
+      </div>
     </div>
   );
 };
